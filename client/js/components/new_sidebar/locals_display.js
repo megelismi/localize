d666ecb43a9403c403f4098b216c2c714e166c55
Megan Smith <meg.eli.smi@gmail.com>
2017-02-13 13:11:31 -0800

    reformat users twitter style

--- client/js/components/new_sidebar/locals_display.js
+++ client/js/components/new_sidebar/locals_display.js
@@ -4,16 +4,16 @@ const LocalsDisplay = (props) => {
 
   if (props.users) {
     return (
-      <div>
+      <div className="user-info">
         {props.users.map((user) => {
           return (
-            <ul className ="user-info-list" key={user.id}>
-              <img className="user-image" src={user.image} />
-              <li className="user-image">{user.first_name}</li>
-              <li className="user-bio">{user.bio}</li>
-              <li><button className="see-user-city" onClick={() => {props.selectUser(user)}}>{user.first_name}{'\'s '}{props.city}</button></li>
-            </ul>
-          )
+              <ul className ="user-image-and-description-container" key={user.id}>
+                <img className="user-image" src={user.image} />
+                <li className="user-name">{user.first_name}</li>
+                <li className="user-bio">{user.bio}</li>
+                <li><button className="see-user-city" onClick={() => {props.selectUser(user)}}>{user.first_name}{'\'s '}{props.city}</button></li>
+              </ul>
+            )
         })}
       </div>
     )
