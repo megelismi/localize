8b18307a7171ae75ffa15dd98373abe63875080f
hjmccain <hannahjmccain@gmail.com>
2017-02-13 18:48:40 -0500

    Merge with older version.

--- client/js/components/new_sidebar/locals_display.js
+++ client/js/components/new_sidebar/locals_display.js
@@ -11,7 +11,7 @@ const LocalsDisplay = (props) => {
                 <img className="user-image" src={user.image} />
                 <li className="user-name">{user.first_name}</li>
                 <li className="user-bio">{user.bio}</li>
-                <li><button className="see-user-city" onClick={() => {props.selectUser(user)}}>{user.first_name}{'\'s '}{props.city}</button></li>
+                <li><button className="see-user-city" onClick={() => {props.selectLocalUser(user)}}>{user.first_name}{'\'s '}{props.city}</button></li>
               </ul>
             )
         })}
