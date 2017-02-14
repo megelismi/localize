9e0141c8bcc31ee75bd00528c10dc84f74d2d3ec
Megan Smith <meg.eli.smi@gmail.com>
2017-02-13 22:52:18 -0800

    divide css up into sections

--- client/js/components/new_sidebar/local_details_display.js
+++ client/js/components/new_sidebar/local_details_display.js
@@ -5,8 +5,8 @@ const LocalDetailsDisplay = (props) => {
     return (
       <div className="sidebar-inner-container">
         <ul>
-          <li>{props.userInfo.first_name}</li>
-          <img src={props.userInfo.image} />
+          <li className="locals-details-name">{props.userInfo.first_name}</li>
+          <img className="locals-details-image" src={props.userInfo.image} />
           <li>{props.userInfo.bio}</li>
         </ul>
       </div>
