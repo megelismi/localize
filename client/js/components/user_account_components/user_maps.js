f49a13876a2fcac67d5fa05f8292e50e330083f2
hjmccain <hannahjmccain@gmail.com>
2017-02-22 10:15:09 -0500

    Change some tabs.

--- client/js/components/user_account_components/user_maps.js
+++ client/js/components/user_account_components/user_maps.js
@@ -4,19 +4,17 @@ import { Link, hashHistory } from 'react-router'
 
 const UserMaps = (props) => {
 
-	return (
-    
-		<div className="maps-list-container">
-			<div className="my-maps-header-container">
-				<h4 className="user-maps-header">My Saved Maps</h4>
-			</div>
-			<ul>
-				<button onClick={() => {hashHistory.push(`/usermap/${props.currentUser.id}`)}}>My Portland</button>
-			</ul>
-		</div>
-	)
+  return (
+
+    <div className="maps-list-container">
+      <div className="my-maps-header-container">
+        <h4 className="user-maps-header">My Saved Maps</h4>
+      </div>
+      <ul>
+        <button onClick={() => {hashHistory.push(`/usermap/${props.currentUser.id}`)}}>My Portland</button>
+      </ul>
+    </div>
+  )
 }
 
 export default UserMaps;
- 
-
