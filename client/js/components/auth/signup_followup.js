bb9a29aa04051e9f77ea0da9eca6a50572821116
hjmccain <hannahjmccain@gmail.com>
2017-02-22 09:13:58 -0500

    Refactor signup_followup.js.

--- client/js/components/auth/signup_followup.js
+++ client/js/components/auth/signup_followup.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as post_actions from '../../actions/post_request.js'; 
 import * as actionCreators from '../../actions/sync.js';
 import { connect } from 'react-redux';
 import { Modal } from 'react-bootstrap';
@@ -7,57 +6,27 @@ import { Modal } from 'react-bootstrap';
 
 export class FollowUpModal extends React.Component {
 
-	constructor (props) {
-		super(props); 
-	}
+  render () {
+    const { followUpModalOpen, signUpFollowUpModal } = this.props;
 
-	sendSignUpInfo (event) {
-		// event.preventDefault(); 
-		// let user = {
-		// 	first_name: this.first_name.value, 
-		// 	last_name: this.last_name.value, 
-		// 	email: this.email.value,
-		// 	username: this.username.value, 
-		// 	password: this.password.value, 
-		// 	confirmed_password: this.confirmed_password.value
-		// }
-
-		console.log('something happened')
-		// this.props.dispatch(post_actions.createNewUser(user)); 
-	}
-
-	// open () {
-	// 	this.props.dispatch(actionCreators.followUpModalOpen());
-	// }
-
-	close () {
-		this.props.dispatch(actionCreators.signUpFollowUpModal());
-	}
-
-	render () {
-
-		const { followUpModalOpen } = this.props;  
-
-		return (
-		<Modal show={followUpModalOpen} onHide={this.close.bind(this)}>
-    	<Modal.Header closeButton>
-      	<Modal.Title>Sign Up</Modal.Title>
-    	</Modal.Header>
-   		<Modal.Body>
-   			<p>I'm the follow up!</p>
-   		</Modal.Body>
-   		<Modal.Footer>
-    	</Modal.Footer>
-  	</Modal>
-		)
-	}
+    return (
+      <Modal show={followUpModalOpen} onHide={() => {signUpFollowUpModal()}}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sign Up</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>I'm the follow up!</p>
+        </Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
 
 }
 
 const mapStateToProps = state => ({
-	followUpModalOpen: state.followUpModalOpen
+  followUpModalOpen: state.followUpModalOpen
 })
 
-export default connect(mapStateToProps)(FollowUpModal);
-
-
+export default connect(mapStateToProps, actionCreators)(FollowUpModal);
