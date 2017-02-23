2f6cfc561e81e6da198db2c98d5f1d86e6856fff
Megan Smith <meg.eli.smi@gmail.com>
2017-02-23 14:01:04 -0800

    feedback text on create map page

--- client/js/components/local_components/new_map_sidebar/save_map.js
+++ client/js/components/local_components/new_map_sidebar/save_map.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { hashHistory } from 'react-router';
 import * as postActionCreators from '../../../actions/post_request.js';
 
-const SaveMap = (props) => {
-
-  const saveUserLocationsToMap = () => {
-    props.localsMapLocations.forEach((location) => {
-      props.saveMap(location);
-    });
+class SaveMap extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      infoText: `Search for locations to pin them to your map, then click the edit icon to add personalized descriptions.`
+    }
   }
 
-  let saveButton = (props.localsMapLocations.length === 0 || props.localsMapLocations.length > props.saveable.length) ?
-    <button className="disabled save-map-button" disabled>Save map</button> :
-    <button onClick={saveUserLocationsToMap} className="save-map-button">Save map</button>
+  saveUserLocationsToMap() {
+    this.props.localsMapLocations.forEach((location) => {
+      this.props.saveMap(location);
+    });
+    this.setState({ infoText: "Yay! Your map locations have been published!" })
+  }
 
-  return (
-    <div>
-      {saveButton}
-    </div>
-  )
+  setInfoText() {
+    this.setState({ infoText: "Whoops — looks like you haven't added descriptions to all of your pinned locations."})
+  }
+
+  render() {
+    return (
+      <div>
+        <h5 className="save-map-text"><span className="save-map-span">
+          {this.state.infoText}
+        </span></h5>
+        {
+          (this.props.localsMapLocations.length === 0 || this.props.localsMapLocations.length > this.props.saveable.length) ?
+            <button
+              onClick={this.setInfoText.bind(this)}
+              className="no-click save-map-button">Save map</button> :
+            <button onClick={this.saveUserLocationsToMap.bind(this)} className="save-map-button">Save map</button>
+        }
+      </div>
+    )
+  }
+
 }
 
 export default connect(null, postActionCreators)(SaveMap);
