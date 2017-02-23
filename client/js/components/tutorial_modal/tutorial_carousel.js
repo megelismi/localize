b0c68d669ced5b25ff1927378703833fa96c1159
Megan Smith <meg.eli.smi@gmail.com>
2017-02-23 00:49:24 -0800

    change wording in tutorial, fixed issues of modal resizing when scrolling through slides

--- client/js/components/tutorial_modal/tutorial_carousel.js
+++ client/js/components/tutorial_modal/tutorial_carousel.js
@@ -9,35 +9,35 @@ const TutorialCarousel = (props) => {
 	      <img width={900} height={500} alt="900x500" src="assets/images/maps-view.png"/>
 	      <Carousel.Caption>
 	        <h3>Explore the Map</h3>
-	        <p>Localize is about bringing you recommendations from those who know a city best, its locals! Click on the map's pins to see pictures and read what city natives have to say about it.</p>
+	        <p>Localize is about bringing you recommendations from those who know a city best, its locals! Click on the map's pins to see pictures and read what city natives have to say.</p>
 	      </Carousel.Caption>
 	    </Carousel.Item>
 	    <Carousel.Item>
 	      <img width={900} height={500} alt="900x500" src="assets/images/locals-view.png"/>
 	      <Carousel.Caption>
 	        <h3>Meet the Locals</h3>
-	        <p>Scroll past the locals in the sidebar to see a picture and short tagline. If you'd like to know more about them, simply click on their name. </p>
+	        <p>Scroll past the locals in the sidebar to see a picture and read a short tagline. If you'd like to know more, simply click on their name. </p>
 	      </Carousel.Caption>
 	    </Carousel.Item>
 	    <Carousel.Item>
 	      <img width={900} height={500} alt="900x500" src="assets/images/local-view.png"/>
 	      <Carousel.Caption>
 	        <h3>Find Out More</h3>
-	        <p>In the locals view, you will see the map change to only show the locations they have pinned and only tags associated with them will show up in the filters.</p>
+	        <p>In the individual view, you will see the map and filters change to be specific to the locations that the selected user has pinned.</p>
 	      </Carousel.Caption>
 	    </Carousel.Item>
 	    <Carousel.Item>
 	      <img width={900} height={500} alt="900x500" src="assets/images/tag-view.png"/>
 	      <Carousel.Caption>
 	        <h3>Narrow Your Search</h3>
-	        <p>Looking for something specific? Click on the filter at the top of the sidebar to use the locals tags to narrow your search. The filter will remian green to remind you that its on.</p>
+	        <p>Looking for something specific? Click on the filter at the top of the sidebar to narrow your search using tags. The filter will remain green until you click clear all.</p>
 	      </Carousel.Caption>
 	    </Carousel.Item>
 	    <Carousel.Item>
 	      <img width={900} height={500} alt="900x500" src="assets/images/create-map.png"/>
 	      <Carousel.Caption>
 	        <h3>Share the Love</h3>
-	        <p>Are you passionate places in your town and want visitors to know? Sign up for an account to create a map of your favorite local spots.</p>
+	        <p>Are you passionate about places in your town and want visitors to know? Sign up to create a map of your favorite spots.</p>
 	      </Carousel.Caption>
 	    </Carousel.Item>
 	  </Carousel>
