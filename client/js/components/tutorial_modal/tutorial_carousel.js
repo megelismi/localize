8744bc83460ffd9ea194c69b1011abe03e37044e
Megan Smith <meg.eli.smi@gmail.com>
2017-02-22 23:03:16 -0800

    style image carousel, add screenshots

--- client/js/components/tutorial_modal/tutorial_carousel.js
+++ client/js/components/tutorial_modal/tutorial_carousel.js
@@ -4,23 +4,37 @@ import { Carousel } from 'react-bootstrap';
 const TutorialCarousel = (props) => {
 
 	return (
-	  <Carousel>
+	  <Carousel interval={false}>
 	    <Carousel.Item>
-	      <img width={900} height={500} alt="900x500" src="http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg"/>
+	      <img width={900} height={500} alt="900x500" src="assets/images/maps-view.png"/>
 	      <Carousel.Caption>
 	        <h3>First slide label</h3>
 	        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
 	      </Carousel.Caption>
 	    </Carousel.Item>
 	    <Carousel.Item>
-	      <img width={900} height={500} alt="900x500" src="http://www.awesomelycute.com/gallery/2015/05/cute-puppies-13.jpg"/>
+	      <img width={900} height={500} alt="900x500" src="assets/images/locals-view.png"/>
 	      <Carousel.Caption>
 	        <h3>Second slide label</h3>
 	        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 	      </Carousel.Caption>
 	    </Carousel.Item>
 	    <Carousel.Item>
-	      <img width={900} height={500} alt="900x500" src="https://i.ytimg.com/vi/a6KGPBflhiM/hqdefault.jpg"/>
+	      <img width={900} height={500} alt="900x500" src="assets/images/local-view.png"/>
+	      <Carousel.Caption>
+	        <h3>Third slide label</h3>
+	        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+	      </Carousel.Caption>
+	    </Carousel.Item>
+	    <Carousel.Item>
+	      <img width={900} height={500} alt="900x500" src="assets/images/tag-view.png"/>
+	      <Carousel.Caption>
+	        <h3>Third slide label</h3>
+	        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+	      </Carousel.Caption>
+	    </Carousel.Item>
+	    <Carousel.Item>
+	      <img width={900} height={500} alt="900x500" src="assets/images/create-map.png"/>
 	      <Carousel.Caption>
 	        <h3>Third slide label</h3>
 	        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
