4d5e92e7857d3da8f08d1304f8f173c81d17b42d
Megan Smith <meg.eli.smi@gmail.com>
2017-02-23 14:13:09 -0800

    tutorial modal popping up on new map page

--- client/js/components/pages/new_map_display.js
+++ client/js/components/pages/new_map_display.js
@@ -3,18 +3,31 @@ import NewMap from '../local_components/new_map/new_map';
 import NewMapSidebar from '../local_components/new_map_sidebar/new_map_sidebar';
 import Header from '../partials/header';
 import Footer from '../partials/footer';
+import { connect } from 'react-redux';
+import Tutorial from '../tutorial_modal/tutorial';
 
-const NewMapDisplay = (props) => {
+class NewMapDisplay extends React.Component {
+	
+	constructor(props) {
+		super(props); 
+	}
 
-  return (
-    <div>
-      <Header />
-      <NewMap />
-      <NewMapSidebar />
-      <Footer />
-    </div>
-  )
+	render () {
+	  return (
+	    <div>
+	      <Header />
+				{this.props.tutorialModalOpen ? <Tutorial /> : null}
+	      <NewMap />
+	      <NewMapSidebar />
+	      <Footer />
+	    </div>
+	  )
+	}
 }
 
 
-export default NewMapDisplay;
+const mapStateToProps = (state) => ({
+  tutorialModalOpen: state.tutorialModalOpen
+});
+
+export default connect(mapStateToProps)(NewMapDisplay);
