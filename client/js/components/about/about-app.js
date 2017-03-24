e08283ad9ae7654655c8c55e2f65b3fc21361ce4
Megan Smith <meg.eli.smi@gmail.com>
2017-03-24 13:11:32 -0700

    formatting added on about div for h2 and hr

--- client/js/components/about/about-app.js
+++ client/js/components/about/about-app.js
@@ -3,6 +3,8 @@ import React from 'react';
 const About = props => {
 	return (
 		<div className="about-page">
+			<h2>How It Works</h2>
+			<hr className="short-line" />
 			<div className="about-cards-container">
 				<div className="about-card">card1</div>
 				<div className="about-card">card2</div>
