b718b8c9ed07bb82041f4f4802e9637ac1861050
Megan Smith <meg.eli.smi@gmail.com>
2017-03-24 15:41:37 -0700

    about descriptions shortened

--- client/js/components/about/about-cards.js
+++ client/js/components/about/about-cards.js
@@ -7,23 +7,21 @@ const AboutCards = () => {
 				<img className="about-icon" src="assets/images/review.png" />
 				<h3>Explore the Reviews</h3>
 				<hr className="short-line cards-line" />
-				<p>Ever been on a trip and found yourself wishing you could pick a local's brain — 
-				find out her favorite eatery or music venue? Localize
-				brings you reviews by the people who know a city best - its locals.</p>
+				<p>Ever been on a trip and found yourself wishing you could pick a local's brain?
+				 Localize brings you reviews by the people who know a city best - its locals.</p>
 			</div>
 			<div className="about-card">
 				<img className="about-icon" src="assets/images/filter.png" />
 				<h3>Filter your Search</h3>
 				<hr className="short-line cards-line" />
-				<p>Sometimes we know exactly what we want. The filtering feature allows you to narrow your search to locations
-				that have been tagged by locals with those keywords. </p>
+				<p>Sometimes we know exactly what we want. The filtering feature allows you to search for locations with that keyword.</p>
 			</div>
 			<div className="about-card">
 				<img className="about-icon" src="assets/images/local.png" />
 				<h3>Learn About the Locals</h3>
 				<hr className="short-line cards-line" />
-				<p>Do you have something in common with one of the locals and want to learn more? 
-				Clicking on their name gives you an extended bio and a filtered view of their locations and tags. </p>
+				<p>Have something in common with one of the users? 
+				Click on her name to see an extended bio and a filtered view of her locations and tags. </p>
 			</div>
 		</div>
 	)
