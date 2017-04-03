db56e12a9798a95d2f1f7a547ae2b432bb932ef0
Megan Smith <meg.eli.smi@gmail.com>
2017-04-02 22:06:18 -0700

    basic styling added to multiple user reviews

--- client/js/components/visitor_components/map_view_sidebar/location_details_display.js
+++ client/js/components/visitor_components/map_view_sidebar/location_details_display.js
@@ -9,21 +9,22 @@ const LocationDetailsDisplay = (props) => {
   return (
  		<div className="location-details-container">
       <button className="close-button" onClick={() => {props.selectLocationById(null)}}>
-        <i className="fa fa-window-close" aria-hidden="true"></i>
+        <img src="/assets/images/close-button.svg" />
       </button>
-      <div className="selected-location-name">{props.locationName}</div>
-      { props.reviewInfo.map(review => {
-				return (
-          <div>
-            <div className="user-info-for-review">
-              <img className="user-image" src={review.user.image} />
-              <p>{review.user.first_name}</p>
+      <div className="selected-location-info-container">
+        <div className="selected-location-name">{props.locationName}</div>
+        { props.reviewInfo.map(review => {
+  				return (
+            <div className="location-details-user-container">
+              <div className="user-info-for-review">
+                <img className="user-image" src={review.user.image} />
+                <p>{review.user.first_name}</p>
+              </div>
+    	      	<div className="selected-location-info">{review.long_description}</div>
             </div>
-  					{ review.image ? <img className="location-image" src={review.image} /> : null }
-  	      	<div className="selected-location-info">{review.long_description}</div>
-          </div>
-				)
-			}) }
+  				)
+  			}) }
+      </div>
     </div>
   )
 }
