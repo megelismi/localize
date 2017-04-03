fe3c3c21c7aecabb8cab1e1b7dd043a2268c10a3
Megan Smith <meg.eli.smi@gmail.com>
2017-04-03 00:25:21 -0700

    when you select a user, it now sets selected location to null in the store

--- client/js/reducer.js
+++ client/js/reducer.js
@@ -52,6 +52,20 @@ const state = (state = {
     case sync_actions.CLEAR_ALL_APPLIED_TAGS: 
     return Object.assign({}, state, {selectedTags: []});
 
+    case sync_actions.SELECT_LOCATION_BY_ID:
+    return state = Object.assign({}, state, {selectedLocation: action.id});
+
+    case sync_actions.DESELECT_USER:
+    return state = Object.assign({}, state, {
+      selectedUser: null,
+      filteredLocations: state.locations,
+      tagsFilteredByUser: null
+    });
+
+    case sync_actions.SELECT_USER:
+    return state = Object.assign({}, state, {selectedUser: action.user, selectedLocation: null});
+
+
     case post_actions.GET_SELECTED_LOCATION_REVIEWS_SUCCESS: 
       let reviews = action.reviews.reviews; 
       let users = state.relevantUsers; 
@@ -193,94 +207,6 @@ const state = (state = {
       logOutError: true
     });
 
-    // case get_actions.GET_USERS_SUCCESS:
-    // return state = Object.assign({}, state, {
-    //   users: action.users,
-    //   usersError: false
-    // });
-
-    // // case sync_actions.SHOW_RELEVANT_USERS_ONLY:
-    // let selectedUsers;
-    // if (state.locationUserTagsHelper && state.users) {
-    //   let idsToSelect = state.allLocationsAndDescriptions.map((relation) => {
-    //     return relation.user_id
-    //   }).filter((item, idx, ary) => ary.indexOf(item) === idx);
-    //   selectedUsers = idsToSelect.map((id) => {
-    //     return state.users.filter((user) => {
-    //       return user.id === id;
-    //     });
-    //   }).reduce((a, b) => a.concat(b));
-    // } else {
-    //   selectedUsers = state.users || [];
-    // }
-    // return state = Object.assign({}, state, {
-    //   oldRelevantUsers: selectedUsers
-    // });
-
-    // case get_actions.GET_USERS_ERROR:
-    // return state = Object.assign({}, state, {
-    //   usersError: true
-    // });
-
-    // case get_actions.GET_TAGS_SUCCESS:
-    // return state = Object.assign({}, state, {
-    //   tagInfoHelper: action.tags,
-    //   tagsError: false
-    // });
-
-    // case get_actions.GET_TAGS_ERROR:
-    // return state = Object.assign({}, state, {
-    //   tagsError: true
-    // });
-
-    // case get_actions.GET_LOCATION_USER_TAGS_HELPER_SUCCESS:
-    // return state = Object.assign({}, state, {
-    //   locationUserTagsHelper: action.location_user_tags,
-    //   locationUserTagsHelperError: false
-    // });
-    // case get_actions.GET_LOCATION_USER_TAGS_HELPER_ERROR:
-    // return state = Object.assign({}, state, {
-    //   locationTagsError: true
-    // });
-
-    // case get_actions.GET_LOCATIONS_SUCCESS:
-    // return state = Object.assign({}, state, {
-    //   oldLocations: action.locations,
-    //   locationsError: false
-    // });
-    // case get_actions.GET_LOCATIONS_ERROR:
-    // return state = Object.assign({}, state, {
-    //   locationsError: true
-    // });
-
-    // case get_actions.GET_DESCRIPTIONS_SUCCESS:
-    // let mergedLocations = action.descriptions.map(description => {
-    //   let merge = description;
-    //   let matches = state.filteredLocations.filter(location => location.id === merge.location_id);
-    //   matches.forEach(match => {
-    //     return merge = Object.assign({}, match, {
-    //       short_description: merge.short_description,
-    //       long_description: merge.long_description,
-    //       image: merge.image,
-    //       user_id: merge.user_id,
-    //       saved: merge.saved,
-    //       show: merge.show || 'yes'
-    //     });
-    //   });
-    //   return merge;
-    // });
-    // let locationsToInclude = mergedLocations.filter(location => {
-    //   return location.show === 'yes';
-    // });
-    // return state = Object.assign({}, state, {
-    //   allLocationsAndDescriptions: locationsToInclude,
-    //   getDescriptionsError: false
-    // });
-    // case get_actions.GET_DESCRIPTIONS_ERROR:
-    // return state = Object.assign({}, state, {
-    //   getDescriptionsError: true
-    // });
-
     case sync_actions.GET_SEARCH_RESULTS:
     return state = Object.assign({}, state,
       { mapzenSelectedResults: [ ...state.mapzenSelectedResults,
@@ -322,125 +248,20 @@ const state = (state = {
         locationsSavedModalOpen: !state.locationsSavedModalOpen
       });
 
-    // case sync_actions.FILTER_TAGS_BY_SELECTED_LOCATIONS:
-    // let relevantTags;
-    // if (state.allLocationsAndDescriptions) {
-    //   let locationsToFilter = state.selectedUserLocations || state.allLocationsAndDescriptions
-    //   let locationIDs = locationsToFilter.map((location) => location.id)
-    //   .filter((item, idx, ary) => ary.indexOf(item) === idx );
-    //   let filteredJoinArrayForTags = locationIDs.map((id) => {
-    //     return state.locationUserTagsHelper.filter((object) => object.location_id === id)
-    //     .map((object) => object.tag_id) })
-    //     .reduce((a, b) => a.concat(b))
-    //     .filter((item, idx, ary) => ary.indexOf(item) === idx );
-    //     relevantTags = filteredJoinArrayForTags.map((id) => {
-    //       return state.tagInfoHelper.filter((tag) => tag.id === id)
-    //     }).reduce((a, b) => a.concat(b));
-    //   } else {
-    //     relevantTags = [];
-    //   }
-    //   return state = Object.assign({}, state, {
-    //     allTags: relevantTags,
-    //     tagsError: false
-    //   });
-
-    // case sync_actions.FILTER_BY_TAG:
-
-    // FILTER_BY_TAG takes all city locations or, if a specific user is selected, it takes
-    // just that user's locations
-    // case sync_actions.FILTER_BY_TAG:
-    // let newTagsArray, selectedLocations;
-    // // modify an array of all currently selected tags
-    // if (state.selectedTags.indexOf(action.tag) === -1) {
-    //   !action.tag ?
-    //   newTagsArray = [] :
-    //   newTagsArray = [ ...state.selectedTags, action.tag ]
-    // } else if (state.selectedTags.indexOf(action.tag) !== -1) {
-    //   let deleteAt = state.selectedTags.findIndex((elem) => elem === action.tag);
-    //   newTagsArray = state.selectedTags.slice(0, deleteAt).concat(state.selectedTags.slice(deleteAt + 1))
-    // } else {
-    //   newTagsArray = [];
-    // }
-    // // find all locations that match any tag in selected tags array
-    // if (newTagsArray.length === 0) {
-    //   selectedLocations = [];
-    //   // selectedLocations = state.locationsFilteredByUser || state.allLocationsAndDescriptions;
-    // } else {
-    //   let locations = state.locationsFilteredByUser || state.allLocationsAndDescriptions;
-    //   let filteredJoinArray = newTagsArray.map((id) => {
-    //     return state.locationUserTagsHelper.filter((object) => {
-    //       return object.tag_id === id
-    //     })
-    //     .map((object) => object.location_id) })
-    //     .reduce((a, b) => a.concat(b))
-    //     .filter((item, idx, ary) => ary.indexOf(item) === idx );
-    //     selectedLocations = filteredJoinArray.map((locationID) => {
-    //       return locations.filter((location) => location.id === locationID);
-    //     }).reduce((a, b) => a.concat(b));
-    //   }
-    //   return state = Object.assign({}, state, {
-    //     selectedTags: newTagsArray,
-    //     selectedLocations: selectedLocations
-    //   });
-
-    case sync_actions.DESELECT_USER:
-    return state = Object.assign({}, state, {
-      selectedUser: null,
-      filteredLocations: state.locations,
-      tagsFilteredByUser: null
-    });
-
-    case sync_actions.SELECT_USER:
-    return state = Object.assign({}, state, {
-      selectedUser: action.user
-    });
-
-    // case sync_actions.FILTER_LOCATIONS_BY_USER:
-    // let selectedUserLocations;
-    // if (action.user) {
-    //   let filteredJoinArrayForUser = state.locationUserTagsHelper.filter((object) => {
-    //     return object.user_id === action.user.id
-    //   });
-    //   selectedUserLocations = filteredJoinArrayForUser.map((object) => {
-    //     return state.allLocationsAndDescriptions.filter((location) => {
-    //       return location.id === object.location_id
-    //     });
-    //   }).reduce((a, b) => a.concat(b))
-    //   .filter((item, idx, ary) => ary.indexOf(item) === idx )
-    //   .filter(item => {
-    //     return item.user_id === action.user.id;
-    //   });
-    // } else {
-    //   selectedUserLocations = state.allLocationsAndDescriptions;
-    // }
-    // return state = Object.assign({}, state, {
-    //   locationsFilteredByUser: selectedUserLocations
+    // case sync_actions.FILTER_TAGS_BY_USER:
+    // let filteredLocationUserTags = state.locationUserTagsHelper.filter((object) => {
+    //   return object.user_id === state.selectedUser.id;
     // });
-
-    case sync_actions.FILTER_TAGS_BY_USER:
-    let filteredLocationUserTags = state.locationUserTagsHelper.filter((object) => {
-      return object.user_id === state.selectedUser.id;
-    });
-    let tagsFilteredByUser = filteredLocationUserTags.map((object) => {
-      return state.allTags.filter((tag) => {
-        return tag.id === object.tag_id;
-      });
-    })
-    .reduce((a, b) => a.concat(b))
-    .filter((item, idx, ary) => ary.indexOf(item) === idx );
-    return state = Object.assign({}, state, {
-      tagsFilteredByUser
-    });
-
-    case sync_actions.CLEAR_ALL_APPLIED_TAGS:
-    return state = Object.assign({}, state, {
-      selectedTags: [],
-      selectedLocations: []
-    });
-
-    case sync_actions.SELECT_LOCATION_BY_ID:
-    // const selected = state.allLocationsAndDescriptions.filter((location) => location.id === action.id);
-    return state = Object.assign({}, state, {selectedLocation: action.id});
+    // let tagsFilteredByUser = filteredLocationUserTags.map((object) => {
+    //   return state.allTags.filter((tag) => {
+    //     return tag.id === object.tag_id;
+    //   });
+    // })
+    // .reduce((a, b) => a.concat(b))
+    // .filter((item, idx, ary) => ary.indexOf(item) === idx );
+    // return state = Object.assign({}, state, {
+    //   tagsFilteredByUser
+    // });
 
     default:
     return state;
